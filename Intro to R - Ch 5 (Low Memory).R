1dd05e087b4aa2cc0707b82bb5d1d4c4f6a180d3
Unknown <troy.adair@datadicts.com>
2019-09-07 08:35:04 -0400

    Revised Chapter 5 Low Memory Version

--- Intro to R - Ch 5 (Low Memory).R
+++ Intro to R - Ch 5 (Low Memory).R
@@ -21,10 +21,12 @@ getwd()
 file.edit(".gitignore")
 
 # Importing the file and measuring how long the import takes
+### Run as a block of text to time #########
 ptm <- proc.time()
 DF <- read.csv("Yellow_Sample.csv")
 CSV_READ_TIME <- (proc.time() - ptm)
 CSV_READ_TIME
+############################################
 
 # Looking at what we got
 class(DF)
@@ -43,12 +45,14 @@ getDTthreads(verbose=TRUE)
 setDTthreads(0)
 getDTthreads()
 
-# Doing a timed read of the same file
+# Doing a timed read of the same file with fread()
+### Run as a block of text to time #########
 ptm <- proc.time()
 DF <- fread("Yellow_Sample.csv", header="auto", 
             data.table=FALSE)
 FREAD_READ_TIME <- (proc.time() - ptm)
 FREAD_READ_TIME
+############################################
 
 # Examining what we got
 class(DF)
@@ -57,6 +61,7 @@ str(DF)
 names(DF)
 
 # Bringing in column headers as names and using them to set names
+### Run as a block of text to time #########
 ptm <- proc.time()
 header <- read.table("Yellow_Sample.csv", header = TRUE,
                      sep=",", nrow = 1)
@@ -66,6 +71,7 @@ setnames(DF, colnames(header))
 rm(header)
 FREAD_READ_TIME <- (proc.time() - ptm)
 FREAD_READ_TIME
+############################################
 
 # Examining what we got again
 class(DF)
@@ -97,10 +103,13 @@ if (!require("readr")) install.packages("readr")
 library("readr")
 
 # A timed example of readr::read_csv()
+### Run as a block of text to time #########
 ptm <- proc.time()
 DF <- read_csv("Yellow_Sample.csv", col_names=TRUE)
 READR_READ_TIME <- (proc.time() - ptm)
 READR_READ_TIME
+############################################
+
 CSV_READ_TIME
 FREAD_READ_TIME
 
@@ -136,19 +145,61 @@ summary(Yellow_Tripdata_2017_06)
 summary(Yellow_Tripdata_2017_06 $ trip_distance)
 #
 # Throwing out "non-fares"
-d2<-Yellow_Tripdata_2017_06[which(
+Yellow_Tripdata_2017_06 <- Yellow_Tripdata_2017_06[which(
   Yellow_Tripdata_2017_06$fare_amount>=0 & 
   Yellow_Tripdata_2017_06$fare_amount<100000),]
-summary(d2 $ fare_amount)
+summary(Yellow_Tripdata_2017_06 $ fare_amount)
 
 # Constraining passenger_count to = 1 or 2
-d3<-Yellow_Tripdata_2017_06[which(
+Yellow_Tripdata_2017_06<-Yellow_Tripdata_2017_06[which(
   Yellow_Tripdata_2017_06$passenger_count==1 | 
-    Yellow_Tripdata_2017_06$passenger_count==2),]
-summary(d3 $ passenger_count)
+  Yellow_Tripdata_2017_06$passenger_count==2),]
+summary(Yellow_Tripdata_2017_06 $ passenger_count)
 
-# Using data.table:fwrite()to save d3 as csv:
-fwrite(d3,"d3.csv")
+# Using data.table:fwrite()to save our curated data as csv:
+fwrite(Yellow_Tripdata_2017_06,"Yellow_Curated.csv")
 
-# Let's save our "original" data frame for the next module...
-save(Yellow_Tripdata_2017_06,file="Yellow_Tripdata_2017_06.RData")
+# Let's re-read our "original" data set,
+# create a sample subset, and save it
+# for the next chapter.
+
+# First, clear memory and the Console 
+rm(list=ls(all=TRUE))
+cat("\014")
+
+# Re-read the csv:
+header <- read.table("Yellow_Sample.csv", header = TRUE,
+                     sep=",", nrow = 1)
+DF <- fread("Yellow_Sample.csv",
+                                 skip=1, sep=",",header=FALSE,
+                                 data.table=FALSE)
+setnames(DF, colnames(header))
+rm(header)
+
+# Save the "bad" observations so we can clean them out
+# in the next chapter
+DF2<-DF[which(DF$total_amount<=0 |
+                DF$fare_amount >=100000 |
+                DF$fare_amount < 0 |
+                DF$trip_distance >= 100),]
+# Reform DF with only the "good" observations
+DF<-DF[which(DF$total_amount >0 &
+                DF$fare_amount <100000 &
+                DF$fare_amount >= 0 &
+                DF$trip_distance < 100),]
+
+# Select a random subsample of 1,000,000 rows
+set.seed(10)
+index <- sample(1:nrow(DF), 1000000, replace=FALSE)
+
+# Look at the index to see it's just row numbers
+head(index)
+
+# Copy the row numbers for the sample only into Yellow_Sample
+Yellow_Sample <- DF[index,]
+
+# Concatenate (or "bind") the random sample and the "bad" ones
+Yellow_Sample <- rbind(DF2,Yellow_Sample)
+
+# Save the data frame as an R data file
+save(Yellow_Sample,file="Yellow_Sample.RData")
