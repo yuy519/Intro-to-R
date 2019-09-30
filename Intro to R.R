6685319e8d414df2b36587fd23a2bc6730e37079
Yuzi Yang <yuy519@lehigh.edu>
2019-09-29 20:23:10 -0400

    commit message

--- Intro to R.R
+++ Intro to R.R
@@ -1,5 +1,5 @@
-# R script to accopany Intro to R
-# Copy for Troy Adair acting as RStudent 2019
+# R script to accompany Intro to R
+# Copy for Yuzi Yang RStudent 2019
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
